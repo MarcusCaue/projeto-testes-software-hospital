96bcc0b98e367eb7f88491106a878a5bcf79094c
Marcus Cauê <cauemarcuscauefb@gmail.com>
2022-10-10 07:43:07 -0300

    Ambientando-se

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ class Main {
   public static void main(String[] args) {
     Hospital hp1 = new Hospital("Hospital da Grécia");
 
-    Paciente p1 = new Paciente("123.456.789-10", "Jonas", "Rua dos Filósofos Brabos");
+    Paciente p1 = new Paciente("123.456.789-10", "Jonas", "Brabos");
     p1.setProcedimento("Alta");
-    Paciente p2 = new Paciente("987.654.321-09", "Amanda", "Rua dos Filósofos Brabos");
+    Paciente p2 = new Paciente("987.654.321-09", "Amanda", "Brabos");
     p2.setProcedimento("Fila de Espera");
-    Paciente p3 = new Paciente("121.343.565-90", "Kleito", "Rua dos Filósofos Brabos");
+    Paciente p3 = new Paciente("121.343.565-90", "Kleito", "Brabos");
     p3.setProcedimento("Internado");
 
     Fisioterapeuta f1 = new Fisioterapeuta("897.323.564-12", "Jô Soares Lima");
@@ -21,7 +21,6 @@ class Main {
     hp1.addFuncionario(f1); hp1.addFuncionario(e1); hp1.addFuncionario(m1);
 
     System.out.println(hp1.getPacientes());
-
     System.out.println(hp1.getFuncionarios());
 
   }
